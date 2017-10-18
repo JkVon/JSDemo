1317d8d303ed064f8db6b1bed71a1eb96d2bb751
JkVon <JkVooon@gmail.com>
2017-10-18 17:46:40 +0800

    A simplified drump kit

--- drum/js/main.js
+++ drum/js/main.js
@@ -1,9 +1,23 @@
-function Instrument(aName, aKeyCode, aSrcPath){
-    var name = aName,
-    keyCode = aKeyCode,
-    srcPath = aSrcPath;
+window.addEventListener("keydown", function(e){
+var audio = document.querySelector("audio[data-key='" + e.keyCode + "']"),
+    key = document.querySelector(".key[data-key='" + e.keyCode + "']"),
+    keys = null,
+    len = null,
+    i = null;
+
+if (!audio) return;
+key.classList.add("playing");
+audio.currentTime = 0;
+audio.play();
+
+function removeTransition(e) {
+    key.classList.remove("playing");
 }
-function Drum()}
-Drum.prototype = {
-    bassDrum: new Instrument("bassDrum", 65, "../src/Drum_Sounds/bass_drum.wav"),
+
+keys = document.querySelectorAll(".key");
+len = keys.length;
+
+for (i=0; i<len; i++) {
+    keys[i].addEventListener("transitionend", removeTransition);
 }
+});
