69aaa322975319a01cbe46b50356274fc05e2829
JkVon <JkVooon@gmail.com>
2017-10-18 17:02:06 +0800

    A simplified version of drum kit

---
+++ drum/js/main.js
@@ -0,0 +1,9 @@
+function Instrument(aName, aKeyCode, aSrcPath){
+    var name = aName,
+    keyCode = aKeyCode,
+    srcPath = aSrcPath;
+}
+function Drum()}
+Drum.prototype = {
+    bassDrum: new Instrument("bassDrum", 65, "../src/Drum_Sounds/bass_drum.wav"),
+}
