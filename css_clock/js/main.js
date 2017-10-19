7dbb92a5e18a6fcf070719b59618b9a26b916b62
JkVon <JkVooon@gmail.com>
2017-10-19 17:18:35 +0800

    version1.0

--- css_clock/js/main.js
+++ css_clock/js/main.js
@@ -1,13 +1,22 @@
-var secondHand = document.querySelector(".hand--second"); 
+var secondHand = document.querySelector(".hand--second"),
+    minuteHand = document.querySelector(".hand--minute"),
+    hourHand = document.querySelector(".hand--hour"); 
+    
 function setClock() {
     var date = new Date(),
-        second = date.getSeconds(),
-        minute = date.getMinutes(),
-        hour = date.getHours();
+    second = date.getSeconds(),
+    minute = date.getMinutes(),
+    hour = date.getHours(),
+    secondDegree = (second/60)*360,
+    minuteDegeree = (minute/60)*360 + second/10,
+    hourDegree = ((hour%12)/12)*360 + minute/2;
+        
+    secondHand.style.transform = "rotate(" + secondDegree + "deg)";
+    minuteHand.style.transform = "rotate(" + minuteDegeree + "deg)";
+    hourHand.style.transform = "rotate(" + hourDegree + "deg)";
+
+    setTimeout(setClock, 1000);
 
 }
 
-
-setClock();
-// secondHand.style.transform = `rotate(90deg)`;
-// console.log(secondHand);
+setTimeout(setClock, 1000);
