f0c950d4650d43b4f36ba6b88950a1d24d6fa038
JkVon <JkVooon@gmail.com>
2017-10-19 15:37:06 +0800

    CSS clock

---
+++ css_clock/js/main.js
@@ -0,0 +1,13 @@
+var secondHand = document.querySelector(".hand--second"); 
+function setClock() {
+    var date = new Date(),
+        second = date.getSeconds(),
+        minute = date.getMinutes(),
+        hour = date.getHours();
+
+}
+
+
+setClock();
+// secondHand.style.transform = `rotate(90deg)`;
+// console.log(secondHand);
